b895a5afe2940f6acb853fb7349dd75d8c906529
JeongDowny <daun5535@naver.com>
2024-11-06 23:14:06 +0900

    Update: oss-wdbc

--- Open-Source-Software/ml01_lab/wdbc_classification_21101225_Jeongdaun.py
+++ Open-Source-Software/ml01_lab/wdbc_classification_21101225_Jeongdaun.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import (datasets, svm, metrics)
 from matplotlib.lines import Line2D # For the custom legend
 
@@ -9,14 +10,14 @@ def load_wdbc_data(filename):
         target        = [] # Shape: (569, )
         target_names  = ['malignant', 'benign']
         feature_names = ['mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness', 'mean compactness', 'mean concavity', 'mean concave points', 'mean symmetry', 'mean fractal dimension',
-                         'radius error', 'texture error', 'perimeter error', 'area error', 'smoothness error', 'compactness error', 'concavity error', 'concave points error', 'symmetry error', 'fractal dimension error',
-                         'worst radius', 'worst texture', 'worst perimeter', 'worst area', 'worst smoothness', 'worst compactness', 'worst concavity', 'worst concave points', 'worst symmetry', 'worst fractal dimension']
+                        'radius error', 'texture error', 'perimeter error', 'area error', 'smoothness error', 'compactness error', 'concavity error', 'concave points error', 'symmetry error', 'fractal dimension error',
+                        'worst radius', 'worst texture', 'worst perimeter', 'worst area', 'worst smoothness', 'worst compactness', 'worst concavity', 'worst concave points', 'worst symmetry', 'worst fractal dimension']
     wdbc = WDBCData()
     with open(filename) as f:
         for line in f.readlines():
             items = line.split(',')
-            wdbc.target.append(items[1])        # TODO #1) Add the true label (0 for M / 1 for others)
-            wdbc.data.append(items[2:])         # TODO #1) Add 30 attributes (as floating-point numbers)
+            wdbc.target.append(0 if items[1] == 'M' else 1)        # TODO #1) Add the true label (0 for M / 1 for others)
+            wdbc.data.append([float(x) for x in items[2:]])         # TODO #1) Add 30 attributes (as floating-point numbers)
         wdbc.data = np.array(wdbc.data)
     return wdbc
 
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     wdbc = load_wdbc_data('data/wdbc.data')     # TODO #1) Implement 'load_wdbc_data()'
 
     # Train a model
-    model = svm.SVC()                           # TODO #2) Find a better classifier (SVC accuracy: 0.902)
+    model = RandomForestClassifier()            # TODO #2) Find a better classifier (SVC accuracy: 0.902)
     model.fit(wdbc.data, wdbc.target)
 
     # Test the model
@@ -33,6 +34,10 @@ if __name__ == '__main__':
     accuracy = metrics.balanced_accuracy_score(wdbc.target, predict)
 
     # TODO #3) Visualize the confusion matrix
+    cm = metrics.confusion_matrix(wdbc.target, predict)
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=wdbc.target_names)
+    disp.plot()
+    plt.show()
 
     # Visualize testing results
     cmap = np.array([(1, 0, 0), (0, 1, 0)])
